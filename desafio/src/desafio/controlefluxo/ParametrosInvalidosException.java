64ff8edb25ae5d1e5b3b6c06161595235f7a1f43
Viviane Canuto <107592877+VivianeCanuto@users.noreply.github.com>
2024-07-29 22:40:12 -0300

    Add files via upload

---
+++ desafio/src/desafio/controlefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package desafio.controlefluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
